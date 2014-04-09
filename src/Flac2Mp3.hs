8f25572daf27fcc813b378085a007d3dfc398a7c
metanota <metanota@gmail.com>
2014-04-10 02:40:42 +0400

    Added single file coding feature

--- src/Flac2Mp3.hs
+++ src/Flac2Mp3.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE MultiWayIf #-}
+
 import Control.Monad           (forM_, liftM)
 import Data.List               (isSuffixOf)
 import GHC.IO.Exception        (ExitCode)
 import System.Cmd              (system)
-import System.Directory        (getDirectoryContents, removeFile)
+import System.Directory        (doesDirectoryExist, doesFileExist, getDirectoryContents, removeFile)
 import System.Environment      (getArgs)
-import System.FilePath         (combine)
+import System.FilePath         (combine, dropExtension)
 
 data Codec = Flac | Wav | Mp3
 
@@ -28,19 +30,23 @@ runCommand :: String -> String -> IO ExitCode
 runCommand c a = system $ c ++ " " ++ wrapArg a
                  where wrapArg x = "\"" ++ x ++ "\""
 
-filterBy :: String -> FilePath -> [FilePath] -> [FilePath]
-filterBy ext dir = reverse . map (combine dir) . filter (isSuffixOf ext)
+filterBy :: String -> [FilePath] -> [FilePath]
+filterBy ext = reverse . filter (isSuffixOf ext)
 
 getFilesFiltered :: FilePath -> String -> IO [FilePath]
 getFilesFiltered path ext = do
-                            files <- getDirectoryContents path
-                            return $ filterBy ext path files
+                            isDir  <- doesDirectoryExist path
+                            isFile <- doesFileExist      path
+                            files  <- if | isDir     -> liftM (map (combine path)) $ getDirectoryContents path
+                                         | isFile    -> return $ [path]
+                                         | otherwise -> return $ []
+                            return $ filterBy ext files
 
 main :: IO ()
 main = do
        path  <- liftM head getArgs
        flacs <- getFilesFiltered path $ extension Flac
        forM_ flacs $ decode Flac
-       wavs  <- getFilesFiltered path $ extension Wav
+       let wavs = map (\flac -> dropExtension flac ++ extension Wav) flacs
        forM_ wavs $ encode Mp3
        forM_ wavs removeFile
